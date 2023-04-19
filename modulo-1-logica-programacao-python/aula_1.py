377b6228b592e2dade5ed7abcdcf0935a33b06dc
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-19 20:35:52 -0300

    first class, I learn about comments in python

---
+++ modulo-1-logica-programacao-python/aula_1.py
@@ -0,0 +1,5 @@
+"""
+DocString, apesar de muitos desenvolvedores usar como comentário de multiplas linhas,
+isso não é um comentário inclusive o interpretador do Python pode lê ela.
+"""
+print('Hello Python')
