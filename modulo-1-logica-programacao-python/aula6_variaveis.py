6160939d3f33dc3b46024f4cd78cfbf5c6672464
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-20 20:42:02 -0300

    I working on the declaring of variables

---
+++ modulo-1-logica-programacao-python/aula6_variaveis.py
@@ -0,0 +1,6 @@
+nome_completo = 'Eduardo Esteves'
+idade = 18
+maior_de_idade = idade >= 18
+
+print('Nome:', nome_completo, 'Idade:', idade)
+print(nome_completo, 'é maior de idade =', maior_de_idade)
