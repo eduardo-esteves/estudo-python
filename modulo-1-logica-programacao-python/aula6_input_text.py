a06148e59b51b23ede4465a099c02d5f91b1f87a
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-21 20:25:02 -0300

    captured user inputs and permormed casting in some cases

---
+++ modulo-1-logica-programacao-python/aula6_input_text.py
@@ -0,0 +1,7 @@
+nome = input('Qual o seu nome? ')
+
+num_1 = input('Digite o primeiro numero ')
+num_2 = input('Digite o número 2 ')
+
+print(f'O seu nome é {nome}')
+print(f'A soma dos numeros é {int(num_1) + int(num_2)}')
