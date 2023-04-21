702b14dcdac3fcb45cda9898ee9b214b4a79518a
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-21 20:47:40 -0300

    formating strings using python's f-string function

---
+++ modulo-1-logica-programacao-python/aula9_format_string.py
@@ -0,0 +1,9 @@
+nome = 'Eduardo Esteves'
+altura=  1.85
+peso = 90
+imc = peso / 1.85 ** 2
+
+linha1 = f'{nome} tem {altura:.2f} de altura'
+
+print(linha1)
+print('pesa', peso, 'quilos e seu IMC é ', imc)
