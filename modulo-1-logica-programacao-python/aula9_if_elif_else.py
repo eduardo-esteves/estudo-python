aebc18fb058611c9a1c540bf779aa3e74ec7c2bf
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-04-21 21:16:27 -0300

    working with if, elif and else conditionals

---
+++ modulo-1-logica-programacao-python/aula9_if_elif_else.py
@@ -0,0 +1,8 @@
+entrada = input ('Deseja "entrar" ou "sair" ')
+
+if (entrada == 'entrar'):
+    print('Você optou por entrar')
+elif (entrada == 'sair'):
+    print('Você optou por sair')
+else:
+    print('Você não digitou as opções corretas')
