5e3f663e1e22c81ed0e0aad62602e3fe9ef9d4cf
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-06-16 22:44:37 -0300

    basic example of split it's similiar to the function explode in PHP

---
+++ modulo-1-logica-programacao-python/aula43_basic_split.py
@@ -0,0 +1,11 @@
+"""
+split e join com list e str
+split - divide uma string em uma (list) conforme o separador
+join - une uma string
+"""
+
+frase = '   Olha só que   , coisa interessante          '
+lista_frases = frase.split()
+
+
+print(lista_frases)
