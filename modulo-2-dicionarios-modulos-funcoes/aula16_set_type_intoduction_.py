552ed98ea72c10d674dd610fbd4eb7d1ab0f1962
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-08-10 23:24:29 -0300

    introduction to set type

---
+++ modulo-2-dicionarios-modulos-funcoes/aula16_set_type_intoduction_.py
@@ -0,0 +1,17 @@
+# Sets - Conjuntos em Python (tipo set)
+# Conjuntos são ensinados na matemática
+# https://brasilescola.uol.com.br/matematica/conjunto.htm
+# Representados graficamente pelo diagrama de Venn
+# Sets em Python são mutáveis, porém aceitam apenas
+# tipos imutáveis como valor interno.
+
+# Criando um set
+# set(iterável) ou {1, 2, 3}
+# s1 = set('Luiz')
+s1 = set()  # vazio
+s1 = {'Eduardo', 'Vitor', 'Larissa', 'Patricia'}  # com dados
+
+print(s1)
+print()
+
+print(type(s1))
