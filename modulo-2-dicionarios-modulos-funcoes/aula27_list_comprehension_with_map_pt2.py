9f7c6f10ca05b2c09f7a48e4e9bab8c0ccc6482b
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-08-28 23:44:43 -0300

    An example of list comprehension more complex with use of ternary condition

---
+++ modulo-2-dicionarios-modulos-funcoes/aula27_list_comprehension_with_map_pt2.py
@@ -0,0 +1,15 @@
+# Mapeamento de dados em list comprehension
+produtos = [
+    {'nome': 'p1', 'preco': 20.0, },
+    {'nome': 'p2', 'preco': 10.0, },
+    {'nome': 'p3', 'preco': 30.0, },
+]
+
+# Aqui eu posso usar a list comprehension (Map) desempacotando cada valor do objeto alterando seu valor do preço.
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.10} if produto['preco'] >= 20 else {**produto}
+    for produto in produtos
+]
+
+# E só para facilitar, podemos ainda desempacotar cada objeto da lista e imprimir com uma quebra de linha
+print(*novos_produtos, sep='\n')
