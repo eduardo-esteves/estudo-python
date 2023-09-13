2ddbcebf0743d6af596f5084bb44a6bdedbdc8e3
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-09-12 22:55:05 -0300

    scope of variables global, local and reusing variable with key word nonlocal in deep function

---
+++ modulo-2-dicionarios-modulos-funcoes/aula42_nonlocal_globals_variables.py
@@ -0,0 +1,12 @@
+def concatenar(string_inicial):
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar=''):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    return interna
+
+
+c = concatenar('a')('b')
+print(c)
