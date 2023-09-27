753dbe9028246332f45d8a883ff15fbf2ec18d18
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-09-27 20:11:08 -0300

    Advancing with the class structure, constructor arguments, and class properties in Python

---
+++ modulo-3-poo/aula_3_advancing_constructor_properties_class.py
@@ -0,0 +1,12 @@
+class Televisao_3:
+    def __init__(self, marca, ano, preco, n_canais):
+        self.marca = marca
+        self.ano = ano
+        self.preco = preco
+        self.n_canais = n_canais
+
+
+t1 = Televisao_3('LG', 2023, 12.500, 120)
+
+print(t1)
+
