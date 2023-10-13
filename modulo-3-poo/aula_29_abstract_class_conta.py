dd65eca513b6edc380a88b0bc0c2b4a4cada8791
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-10-13 19:51:53 -0300

    Refactored abstract class Conta to best practice

--- modulo-3-poo/aula_29_abstract_class_conta.py
+++ modulo-3-poo/aula_29_abstract_class_conta.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
 
+# 57
 class Conta(ABC):
-
-    _saldo = 0
 
     def __init__(self, agencia, conta, saldo):
         self._agencia = agencia
@@ -10,6 +9,8 @@ class Conta(ABC):
 
         if saldo >= 0:
             self._saldo = saldo
+        else:
+            raise ValueError('O saldo não pode ser negativo')
 
     def get_info(self):
         return f"Agência: {self._agencia}, Conta: {self._conta}"
@@ -17,6 +18,8 @@ class Conta(ABC):
     def depositar(self, quantia):
         if quantia > 0:
             self._saldo += quantia
+        else:
+            raise ValueError('A quantia do depósito deve ser um valor positivo')
 
     def get_saldo(self):
         return self._saldo
