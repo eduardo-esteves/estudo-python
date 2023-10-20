8d4feb1bf6e6443aa4c508e61a7341f09599d4dc
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-10-19 23:52:41 -0300

    Making condition to validate if the script that's being executed is the main one, and if so, the main function will be executed

---
+++ modulo-4-estudo-de-modulos/modulos/modulos_diversos.py
@@ -0,0 +1,13 @@
+def soma(x: float, y: float) -> float:
+    return x + y
+
+def main() -> None:
+    if __name__ == '__main__':
+        print(__name__)
+
+# Verifico se o script principal que está sendo executado no momento
+# é este, em caso positivo ele será o main e com isso a função main()
+# será executada, porém se estiver sendo executada através de outro script
+# via importação então este script não será o principal main.
+if __name__ == '__main__':
+    main()
