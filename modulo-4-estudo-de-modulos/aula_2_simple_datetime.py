df3444b7cbafff20863260225f141eaf7b9b4372
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-10-23 22:25:37 -0300

    Developed a simple datetime with a year specified

---
+++ modulo-4-estudo-de-modulos/aula_2_simple_datetime.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from helper.print_msg_section import print_msg_section
+
+# CPBA 274
+data = datetime(2022, 4, 20)
+
+print_msg_section('Conferindo o tipo datetime object')
+print(type(data))
+print(data)
+
+
