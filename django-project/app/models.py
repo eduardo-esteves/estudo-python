1fc38b7e27e2e379fcfbdd2a41fc9579bc660d86
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-11-07 22:22:08 -0300

    I created my first model Product and run the command "python manage.py makemigrations" to genarate the migration

--- django-project/app/models.py
+++ django-project/app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('Name of Product', max_length=200)
+    price = models.DecimalField('Price', decimal_places=2, max_digits=10)
+    qtd_stock = models.IntegerField('Quantity in Stock')
