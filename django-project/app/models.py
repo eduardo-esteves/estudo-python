c70b813625d830f2e2b8538863e2d33cff853906
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-11-08 20:50:36 -0300

    I created a new Client model

--- django-project/app/models.py
+++ django-project/app/models.py
@@ -6,3 +6,8 @@ class Product(models.Model):
     name = models.CharField('Name of Product', max_length=200)
     price = models.DecimalField('Price', decimal_places=2, max_digits=10)
     qtd_stock = models.IntegerField('Quantity in Stock')
+
+class Client(models.Model):
+    first_name = models.CharField('First Name', max_length=200)
+    last_name = models.CharField('Last Name', max_length=200)
+    email = models.EmailField('Email', max_length=250)
