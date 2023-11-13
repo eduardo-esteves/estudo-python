e0a153b4978460606759dd432967db05c21c9c6b
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-11-13 19:38:19 -0300

    Creating first table named customers

--- modulo-5-databases/aula_1_sqlite_connection.py
+++ modulo-5-databases/aula_1_sqlite_connection.py
@@ -9,5 +9,15 @@ DB_FILE = Path(ROOT_DIR, 'modulo5.sqlite3')
 con = sqlite3.connect(DB_FILE)
 cursor = con.cursor()
 
+# SQL Commands Here
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS customers ('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL)'
+)
+
+con.commit()
+
 cursor.close()
 con.close()
