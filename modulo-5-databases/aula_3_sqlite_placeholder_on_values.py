a14056b78cc66601a428bbc270c5580eee920d20
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-11-14 22:11:32 -0300

    Inserting many values into customers table with executemany and preparing values with binds

---
+++ modulo-5-databases/aula_3_sqlite_placeholder_on_values.py
@@ -0,0 +1,39 @@
+import sqlite3
+from pathlib import Path
+
+
+# CPBA 390
+ROOT_DIR = Path(__file__).resolve().parent
+DB_FILE = Path(ROOT_DIR, 'modulo5.sqlite3')
+table_name = 'customers'
+
+con = sqlite3.connect(DB_FILE)
+cursor = con.cursor()
+
+values = [
+    ["Eduardo Esteves", 92],
+    ["Vitor Esteves", 30]
+]
+
+# SQL Commands Here
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {table_name} ('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL)'
+)
+
+con.commit()
+
+cursor.executemany(
+    f'INSERT INTO {table_name}'
+    '(name, weight)'
+    'VALUES '
+    '(?, ?)',
+    values
+)
+
+con.commit()
+
+cursor.close()
+con.close()
