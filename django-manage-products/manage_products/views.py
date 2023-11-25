08daebb0a357f9160817de1661158830187743dd
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-11-24 23:17:50 -0300

    Instanced ContactForm in contact function view and passed by context to contact template

--- django-manage-products/manage_products/views.py
+++ django-manage-products/manage_products/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
+from django.contrib import messages
 
+from .forms import ContactForm
 
 def index(request):
     return render(request, 'index.html')
 
 
 def contact(request):
-    return render(request, 'contact.html')
+    form = ContactForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            nome = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+
+            messages.success(request, 'Enviado com sucesso!')
+            form = ContactForm()
+        else:
+            messages.error(request, 'Erro ao enviar o e-mail')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'contact.html', context)
 
 
 def products(request):
