be75b29810ba6f345069390e109e16988557dced
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-11-28 20:36:16 -0300

    Saving the POST data received by the product form with the assistance of the newly created ProductModelForm

--- django-manage-products/manage_products/views.py
+++ django-manage-products/manage_products/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from .forms import ContactForm
+from manage_products.forms import ContactForm, ProductModelForm
 
 def index(request):
     return render(request, 'pages/index.html')
@@ -24,5 +24,17 @@ def contact(request):
     return render(request, 'pages/contact.html', context)
 
 
-def products(request):
-    return render(request, 'pages/products.html')
+def product(request):
+    if str(request.method) == 'POST':
+        form = ProductModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Produto salvo com sucesso!')
+            return redirect('index')
+        else:
+            messages.error(request, 'Erro ao salvar produto')
+    form = ProductModelForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'pages/product.html', context)
