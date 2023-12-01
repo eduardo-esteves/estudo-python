fea369d74a90f25627b6669f97f78c85b3615815
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-11-30 21:19:30 -0300

    Renamed the urls products to product

--- django-manage-products/manage_products/urls.py
+++ django-manage-products/manage_products/urls.py
@@ -6,5 +6,5 @@ from manage_products import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact', views.contact, name='contact'),
-    path('products', views.products, name='products'),
+    path('product', views.product, name='product'),
 ]
