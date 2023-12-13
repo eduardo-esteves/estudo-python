3a4487360c3717967503f7f7a8c88c23213c5b1f
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-12-12 22:02:37 -0300

    Registered my models to show in django admin page

--- django-fusion/fusion/admin.py
+++ django-fusion/fusion/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from fusion.models import Position, Services, Team_Members
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('job_title', 'is_active', 'update_at')
+
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ('service', 'description', 'icone', 'is_active', 'update_at')
+
+
+@admin.register(Team_Members)
+class Team_MembersAdmin(admin.ModelAdmin):
+    list_display = ('professional', 'job_title', 'bio', 'is_active', 'update_at')
