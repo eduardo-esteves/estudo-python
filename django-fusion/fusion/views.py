f209316318f061b51c29d8a03a52a966de2c2e73
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-12-14 22:31:37 -0300

    Replaced the TemplateView class with the FormView to enable Django to send email from the form

--- django-fusion/fusion/views.py
+++ django-fusion/fusion/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from fusion.models import Services, Team_Members
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
+from fusion.models import Services, Team_Members
+from fusion.forms import ContactForm
 
 # Create your views here.
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
@@ -13,3 +18,12 @@ class IndexView(TemplateView):
         context['employees'] = Team_Members.objects.order_by('?').all()
 
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_mail()
+        messages.success(self.request, 'E-mail enviado com sucesso!')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form)
