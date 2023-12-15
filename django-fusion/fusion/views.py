f7ea98c129ec78a655879b99fc6c9b35e0741ff8
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-12-14 21:47:22 -0300

    Loaded data into context variable for display in services_area.html and team_area.html dynamically

--- django-fusion/fusion/views.py
+++ django-fusion/fusion/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from fusion.models import Services, Team_Members
 
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Services.objects.order_by('?').all()
+        context['employees'] = Team_Members.objects.order_by('?').all()
+
+        return context
