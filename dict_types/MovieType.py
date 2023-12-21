fa883894b2447766b70f7a997c4aee2285e7f7c1
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2023-12-21 20:15:06 -0300

    I created a directory named dict_types for typing all structure of dictionary object

---
+++ dict_types/MovieType.py
@@ -0,0 +1,11 @@
+from typing import TypedDict
+
+
+class MovieType(TypedDict):
+    title: str
+    original_title: str
+    is_movie: bool
+    imdb_rating: float
+    year: int
+    characters: list[str]
+    budget: None | float
